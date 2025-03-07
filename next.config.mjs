11588daa14df72476e543c905291d9e9b5b4404e
sdelahaies <sdelahaies@gmail.com>
2025-03-07 08:53:53 +0100

    fix: enable CORS

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,22 @@ const nextConfig = {
         // Disable ESLint during build validation
         ignoreDuringBuilds: true,
       },
-
+      headers() {
+        return [
+        //   {
+        //     source: '/(.*)',
+        //     headers: securityHeaders,
+        //   },
+          {
+            source: '/',
+            headers: [
+              { key: 'Access-Control-Allow-Origin', value: '*' },
+              { key: 'Access-Control-Allow-Methods', value: 'GET,HEAD,PUT,PATCH,POST,DELETE' },
+              { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+            ],
+          },
+        ];
+      },
 
 };
 
